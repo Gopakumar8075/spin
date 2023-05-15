5a699a84a16e98aee9fecf20a311995990d779c8
Gopakumar <gopakumar@tiltlabs.io>
2023-05-15 13:45:16 +0530

    changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
     <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
         <div>
             <div className="flex items-center justify-between py-3 md:py-5 md:block">
-                <a href="javascript:void(0)">
+                <a href="">
                     <h2 className="text-2xl font-bold">SPIN IT</h2>
                 </a>
                 <div className="md:hidden">
